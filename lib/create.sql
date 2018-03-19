66cfbe6213775f7428f6d8da673b3c917cb714ec
Rani Shah <rani76@gmail.com>
2018-03-19 04:05:32 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender CHAR(1),
+  gender CHAR(1)(The choices would be "M" or "F"),
   color TEXT,
   temperament TEXT,
   alive BOOLEAN);
