5bac3e75de245e841e20bc15bc51ef935bffce4d
Rani Shah <rani76@gmail.com>
2018-03-19 04:10:12 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,18 +7,3 @@ CREATE TABLE bears (
   temperament TEXT,
   alive BOOLEAN);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr Chocolate", 24, F, "Brown", "Moody", True);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 25, F, "Brown", "Moody", True);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabith", 24, M, "White", "Happy", False);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 28, F, "Brown", "Moody", True);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 70, F, "Black", "Happy", True);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 34, M, "Black", "Moody", False);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 23, M, "Brown", "Happy", True);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 22, F, "White", "Moody", True);
